1c979570414336e826f44016a29258b7d2ed6b2d
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-10-07 15:28:31 +0900

    chore: install react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 
@@ -73,6 +74,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
